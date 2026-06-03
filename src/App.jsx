e65692b901b7e6be30407ac84e0fb4b93e9136a3
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-03 12:44:31 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBarContainer from "./components/NavBarContainer";
 import ProductListContainer from "./components/ProductListContainer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBarContainer></NavBarContainer>
       <ProductListContainer></ProductListContainer>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <section className="flex justify-center p-10">
+      <p>&copy; boarder 2026</p>
+    </section>
+  );
+}
+
+export default Footer;
