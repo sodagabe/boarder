6f4543086e1c7393ae598e7a3d266ff06082181b
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-13 19:14:51 -0500

    Added links to product cards that lead to corresponding detail pages

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router";
+
 function Product({ product }) {
   const { thumbnailURL, title, description, price } = product;
   return (
-    <article className="flex flex-col bg-gray-100 p-4">
-      <img className="w-30" src={thumbnailURL} alt="" />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>{price}</p>
-    </article>
+    <Link to={`/product/${product.id}`}>
+      <article className="flex flex-col bg-gray-100 p-4">
+        <img className="w-30" src={thumbnailURL} alt="" />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>{price}</p>
+      </article>
+    </Link>
   );
 }
 
