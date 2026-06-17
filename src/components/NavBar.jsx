75d7be1c00ca8ff19ec518dc8871518bd8ed0066
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-16 22:55:03 -0500

    Improved header alignment and cart count

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,17 +2,21 @@ import { NavLink } from "react-router";
 
 function NavBar({ brandName, cartQty }) {
   return (
-    <header className="flex grow justify-center">
-      <div className="flex grow gap-8 p-4">
+    <header className="flex h-20 grow justify-center">
+      <div className="flex h-full grow items-center gap-8 p-4">
         <div className="grow text-2xl font-extrabold uppercase">
           <span className="h-full">
             <NavLink to={"/"}>{brandName}</NavLink>
           </span>
         </div>
         <NavLink to="/cart">
-          <div className="flex gap-1">
+          <div className="flex h-full items-center gap-2">
             <span>Cart</span>
-            <span className="font-bold">{cartQty}</span>
+            {cartQty > 0 && (
+              <span className="rounded-sm bg-black px-2 py-1 font-bold text-white">
+                {cartQty}
+              </span>
+            )}
           </div>
         </NavLink>
       </div>
