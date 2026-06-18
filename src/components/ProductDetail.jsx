8a0d0a77c0faf1f2a16cf93e4b03194ed3832dda
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-18 04:25:10 -0500

    Fixed product detail

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -2,21 +2,33 @@ import Button from "./Button";
 
 function ProductDetail({ product, category, handler }) {
   return (
-    <section className="flex">
-      <div>
-        <img src={product.imageURL} alt="" />
-      </div>
-      <div className="flex flex-col p-4">
-        <h2 className="text-5xl">{product.title}</h2>
-        <p>
-          {product.min_players} - {product.max_players} players
-        </p>
-        <p>
-          {product.min_time} - {product.max_time} minutes
-        </p>
-        <p>{product.min_age} and up</p>
-        <p>{category.name}</p>
-        <p className="currency">{product.price}</p>
+    <section className="flex h-max grow flex-col sm:flex-row">
+      <img
+        className="h-80 w-full rounded-xl bg-neutral-900 object-contain sm:aspect-square sm:h-120 sm:w-120 lg:h-150 lg:w-150"
+        src={product.imageURL}
+        alt=""
+      />
+      <div className="flex flex-col gap-8 px-8 sm:grow">
+        <h2 className="text-5xl font-bold">{product.title}</h2>
+        <p className="tracking-widest uppercase">{category.name}</p>
+        <div className="flex flex-col gap-2">
+          <p>
+            <span className="font-semibold">
+              {product.min_players} to {product.max_players}
+            </span>{" "}
+            players
+          </p>
+          <p>
+            <span className="font-semibold">
+              {product.min_time} to {product.max_time}
+            </span>{" "}
+            minutes
+          </p>
+          <p>
+            Ages <span className="font-semibold">{product.min_age} and up</span>
+          </p>
+        </div>
+        <p className="currency text-2xl font-bold">{product.price}</p>
         <Button label="Add to cart" handler={handler} />
       </div>
     </section>
