f6a1d655e6d059267952939ca4121c9ffc421918
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-18 03:44:25 -0500

    Fixed non-empty cart

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -3,7 +3,7 @@ import CartLineItem from "./CartLineItem";
 
 function CartTotal({ itemsSubtotal, shipping, tax, total, buttonHandler }) {
   return (
-    <section className="flex h-fit w-[25%] flex-col rounded-xl bg-gray-100 p-4">
+    <section className="flex h-fit w-[25%] flex-col rounded-xl bg-neutral-100 p-6">
       <div className="pb-8">
         <CartLineItem label="Items" value={itemsSubtotal} />
         <CartLineItem label="Tax" value={tax} />
