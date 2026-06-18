7bf377cae62ba361a685b23532a0beb58a35d5b3
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-18 10:49:02 -0500

    Fixed focus header syling

--- src/components/FocusHeader.jsx
+++ src/components/FocusHeader.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router";
 
 function FocusHeader() {
   return (
-    <header className="flex h-20 w-full justify-center">
+    <header className="flex h-20 w-full justify-center shadow-lg">
       <div className="flex grow items-center justify-center text-2xl font-extrabold uppercase lg:max-w-7xl">
-        <span>
+        <span className="text-primary-500">
           <NavLink to={"/"}>boarder</NavLink>
         </span>
       </div>
