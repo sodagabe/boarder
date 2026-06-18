b63d61dfaec7b859efa63de43f592f1b91c95d90
Gabriel Tovar <gabriel.tovar@pucp.pe>
2026-06-17 21:44:38 -0500

    Added layout for checkout and other directed user flows

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 function Footer() {
   return (
-    <section className="flex justify-center p-10">
-      <p>&copy; boarder 2026</p>
-    </section>
+    <footer className="flex w-full justify-center">
+      <div className="flex max-w-7xl grow flex-col items-center px-6 py-10 text-center">
+        <p>&copy; boarder 2026</p>
+      </div>
+    </footer>
   );
 }
 
